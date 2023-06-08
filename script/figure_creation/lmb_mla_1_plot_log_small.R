a276ddc87c590e95ed09a73164339d077ea8fdec
Tyler Steven Coleman <tscoleman3@gmail.com>
2023-06-08 15:14:00 -0400

    update

---
+++ script/figure_creation/lmb_mla_1_plot_log_small.R
@@ -0,0 +1,84 @@
+
+dat = read.csv("data/lmb_output.csv")
+dat$new = ifelse(dat$lake %in% c("drummond3", "lm", "promise", "mark"), 1, 0)
+dat$drop = ifelse(dat$lake %in% c("lee county lake", "barbour county lake", "washington county lake"), 1, 0)
+
+dat$mla_1[is.na(dat$mla_1)] = 0       # add a 0 in place of NA for removal
+dat = dat[dat$mla_1 != 0,]            # remove 0 in mla_1
+unique(dat$mla_1)                     # 0's and NA's absent 
+dat$mla_1 = log(dat$mla_1)
+
+yvar = "mla_1"
+
+datsub = dat[,c("year", "type", "times_treat", "size_class", "new", "drop", yvar)]
+
+library(dplyr)
+
+plot_dat_sml = datsub %>% filter(size_class == "small") %>%
+  group_by(year, type, new, times_treat) %>%
+  summarize(
+    mn = mean(mla_1, na.rm = T),
+    lwr = quantile(mla_1, 0.025, na.rm = T),
+    upr = quantile(mla_1, 0.975, na.rm = T)
+    ) %>% ungroup
+plot_dat_big = datsub %>% filter(size_class == "big") %>%
+  group_by(year, type, drop, times_treat) %>%
+  summarize(
+    mn = mean(mla_1, na.rm = T),
+    lwr = quantile(mla_1, 0.025, na.rm = T),
+    upr = quantile(mla_1, 0.975, na.rm = T)
+    ) %>% ungroup
+
+plot_dat_sml$year = as.numeric(plot_dat_sml$year)
+plot_dat_big$year = as.numeric(plot_dat_big$year)
+
+
+
+off_c1 = -0.0975
+off_c2 = -0.0325
+off_t2 = 0.0325
+off_t1 = 0.0975
+ppi = 600
+png("writing/figures/lmb_mla_1_log_small.png", h = 3.5 * ppi, w = 4 * ppi, res = ppi)
+
+# small lakes
+par(mfrow = c(1,1), mar = c(0.5,1,0.5,1), oma = c(1.5,3,0.5,0), cex.axis = 1, cex.lab = 1.2, mgp = c(1,0.75,0))
+plot(1,1, type = "n", xlim = c(2017,2019) + range(c(off_c1, off_c2, off_t1, off_t2)), ylim = c(4.9,5.7),
+     xaxt = "n", yaxt = "n", las = 1, ylab = "")
+usr = par("usr"); xdiff = usr[2] - usr[1]; ydiff = usr[4] - usr[3]
+text(x = usr[2] - xdiff * 0, y = usr[4] - ydiff * 0.05, pos = 2, "Largemouth Bass", font = 4)
+
+# controls (not new entries)
+ind = c(1,3,7)
+lines(mn ~ I(year + off_c1), data = plot_dat_sml[ind,], lwd = 2, lty = 2, type = "l")
+with(plot_dat_sml[ind,], segments(I(year + off_c1), lwr, I(year + off_c1), upr, lty = 2, lwd = 2))
+points(mn ~ I(year + off_c1), data = plot_dat_sml[ind,], pch = 21, bg = "white", cex = 1.2)
+
+# treats (not new entries)
+ind = c(2,6,10)
+lines(mn ~ I(year + off_t1), data = plot_dat_sml[ind,], lwd = 2, type = "l")
+with(plot_dat_sml[ind,], segments(I(year + off_t1), lwr, I(year + off_t1), upr, lwd = 2))
+points(mn ~ I(year + off_t1), data = plot_dat_sml[ind,], pch = 21, bg = c("white", "black", "black"), cex = 1.2)
+
+# controls (new entry 2018)
+ind = c(4,8)
+lines(mn ~ I(year + off_c2), data = plot_dat_sml[ind,], lwd = 2, lty = 2, type = "l")
+with(plot_dat_sml[ind,], segments(I(year + off_c2), lwr, I(year + off_c2), upr, lty = 2, lwd = 2))
+points(mn ~ I(year + off_c2), data = plot_dat_sml[ind,], pch = 21, bg = "white", cex = 1.2)
+
+# treats (new entry 2018)
+ind = c(5,9)
+lines(mn ~ I(year + off_t2), data = plot_dat_sml[ind,], lwd = 2, type = "l")
+with(plot_dat_sml[ind,], segments(I(year + off_t2), lwr, I(year + off_t2), upr, lwd = 2))
+points(mn ~ I(year + off_t2), data = plot_dat_sml[ind,], pch = 21, bg = c("white", "black"), cex = 1.2)
+
+legend(usr[1], usr[4] - 0.05, legend = c("Control", "Treatment", "Untreated", "Treated"), ncol = 2,
+       pch = c(NA, NA, 21, 21), pt.cex = c(NA, NA, 1.2, 1.2), pt.bg = c(NA, NA, "white", "black"),
+       bty = "n", lty = c(2,1,NA,NA), lwd = c(2,2,NA,NA), x.intersp = c(0.5, 0.5, 0.25, 0.25))
+
+axis(side = 1, at = 2017:2019, labels = 2017:2019)
+axis(side = 2, at = c(5, 5.5), labels = c(5, 5.5), las = 1)
+mtext(side = 2, outer = T, "ln Age-1 Mean TL (mm)", line = 1.75, cex = 1.2) 
+dev.off()
+
+
